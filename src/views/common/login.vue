d72f7f5c54913bc3898b0f891b83282435b0b09b
chollima <chollima_zhou@163.com>
2022-07-29 04:38:12 +0000

    update src/views/common/login.vue.

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -3,8 +3,8 @@
     <div class="site-content__wrapper">
       <div class="site-content">
         <div class="brand-info">
-          <h2 class="brand-info__text">renren-fast-vue</h2>
-          <p class="brand-info__intro">renren-fast-vue基于vue、element-ui构建开发，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
+          <h2 class="brand-info__text">谷粒商城后台管理系统</h2>
+          <p class="brand-info__intro">提供谷粒商城后台管理的相关服务，包括系统管理、商品管理、优惠营销、库存管理、订单管理、用户管理、内容管理等功能。</p>
         </div>
         <div class="login-main">
           <h3 class="login-title">管理员登录</h3>
